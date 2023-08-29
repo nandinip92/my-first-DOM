33e017472d849e9ef2cba94e487d6e7b2d9d34bf
nandinip92 <nandini.p8892@gmail.com>
2023-08-29 08:09:58 +0100

    feat: adding feedbacks with respective to the strength of the password

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const updatePasswordProgressBar = (length) => {
   document.getElementById("pwd_progress_bar").value = sum;
 
   updateProgressBarColor(length);
+  giveFeedback(length);
 };
 
 const updateProgressBarColor = (length) => {
   const progress = document.getElementById("pwd_progress_bar");
   const color = length < 8 ? "red" : length < 12 ? "orange" : "green";
   progress.style.setProperty("--c", color);
+};
+
+const giveFeedback = (length) => {
+  const progress = document.getElementById("pwd_progress_bar");
+  let strength =
+    length < 8 ? "Should be longer" : length < 12 ? "Pretty good" : "Grrrreat!";
+  if (length === 0) strength = "";
+
+  document.getElementById("pwd_strength").textContent = strength;
 };
 
 const checkPassword = () => {
