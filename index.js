5b28ad045ea75e4644fcb31289093ddc554eb7c2
nandinip92 <nandini.p8892@gmail.com>
2023-09-06 20:44:04 +0100

    fix: fixing the progress bar value logic i.e., number of characters multiplied by 5

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const updatePasswordProgressBar = (length) => {
   const progressBarValue = document.getElementById("pwd_progress_bar").value;
-  //Sum of n consecutive numbers is n(n+1)/2 as per requirement so multiply by 5
-  // So for this 5n*(n+1)/2
-  sum = (5 * length * (length + 1)) / 2;
-  document.getElementById("pwd_progress_bar").value = sum;
+
+  sum = 5 * length;
+  document.getElementById("pwd_progress_bar").value = sum <= 100 ? sum : 100;
 
   updateProgressBarColor(length);
   giveFeedback(length);
